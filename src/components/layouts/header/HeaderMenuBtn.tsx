f0d4ba14259c0854e5555950cbcdba4642d0f1d8
Min's Git <99301568+dattko@users.noreply.github.com>
2024-10-21 02:10:50 +0900

    헤더 스타일 수정. 버튼 액티브 추가

--- src/components/layouts/header/HeaderMenuBtn.tsx
+++ src/components/layouts/header/HeaderMenuBtn.tsx
@@ -1,14 +1,22 @@
 'use client'
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './Header.module.scss';
 import { Menu } from 'lucide-react';
 import { useHeaderStore } from './headerStore';
+import { usePathname } from 'next/navigation';
 
 const HeaderMenuBtn: React.FC = () => {
   const toggleNav = useHeaderStore((state) => state.toggleNav);
+  const isNavActive = useHeaderStore((state) => state.isNavActive);
+  const resetNav = useHeaderStore(state => state.resetNav)
+  const pathname = usePathname();
+
+  useEffect(() => {
+    resetNav();
+  }, [pathname, resetNav]);
 
   return (
-    <button className={styles.header__nav__btn} onClick={toggleNav}>
+    <button className={`${styles.header__nav__btn} ${isNavActive ? styles.active : ''}`} onClick={toggleNav}>
       <Menu  size={20}/>
     </button>
   )
